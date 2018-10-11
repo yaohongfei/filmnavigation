eeeb53022e69c362a01c7436a331d18b7824ad5b
yaohongfei <Hongfei.Yao@acxiom.com>
2018-10-11 17:22:08 +0800

    add file

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,32 @@
+import  { Component } from '@angular/core';
+import { Service } from '../../service/service';
+import { Router } from '@angular/router'
+
+@Component({
+    selector : 'fn-header',
+    templateUrl : './header.component.html',
+    styleUrls : ['./header.component.scss']
+})
+
+export class HeaderComponent {
+    
+    
+    
+    public searchCondition : string;
+
+    constructor(private service : Service,
+    private router : Router) {
+
+    }
+
+    public searchFilms() {
+        this.service.getRequest('film/search/byText/' + this.searchCondition)
+        .then((result : any) => {
+            if (result && result.length > 0) {
+                
+            }
+    })
+
+} 
+
+}
